983316cdf3188e40763db7dc12e640c7a7ca4de0
christiantc <christiantambo15@gmail.com>
2021-03-12 13:49:45 -0400

    CRUD: Create data

--- src/main/java/MessageDAO.java
+++ src/main/java/MessageDAO.java
@@ -1,6 +1,25 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MessageDAO {
     public static void createMessageDB(Message message){
-
+        Connect dbConnect = new Connect();
+        try(Connection connection = dbConnect.getConnection()) {
+            PreparedStatement ps = null;
+            try{
+                String query = "INSERT INTO `message` (`message`, `author_message`) VALUES (?, ?);";
+                ps = connection.prepareStatement(query);
+                ps.setString(1, message.getMessage());
+                ps.setString(2, message.getAuthorMessage());
+                ps.executeUpdate();
+                System.out.println("Message created");
+            }catch (SQLException ex){
+                System.out.println(ex);
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
     }
 
     public static void readMessageDB(){
--- src/main/java/MessageService.java
+++ src/main/java/MessageService.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class MessageService {
     public static void createMessage(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write your message");
+        String message = sc.nextLine();
+
+        System.out.println("Your name");
+        String name = sc.nextLine();
+
+        Message reg = new Message();
+        reg.setMessage(message);
+        reg.setAuthorMessage(name);
+
+        MessageDAO.createMessageDB(reg);
 
     }
     public static void listMessage(){
